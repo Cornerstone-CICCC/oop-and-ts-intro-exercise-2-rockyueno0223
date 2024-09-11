218d32bbf20a17cbea5cfc86eb43714df8b57429
Hiroki Ueno <rockyueno@gmail.com>
2024-09-11 16:00:47 -0700

    exercise1

--- exercise1.js
+++ exercise1.js
@@ -1,5 +1,5 @@
 // Exercise 1: Inheritance, Super, and Private Properties
-// 
+//
 // 1. Create a class `Vehicle` with private properties `#make`, `#model`, and `#year`.
 // 2. The `Vehicle` class should have a method `getInfo` that returns a string with the vehicle’s details.
 // 3. Create a subclass `Car` that inherits from `Vehicle`. Add a new private property `#doors`.
@@ -9,10 +9,36 @@
 
 class Vehicle {
   // YOUR CODE HERE
+
+  #make;
+  #model;
+  #year;
+
+  constructor(make, model, year) {
+    this.#make = make;
+    this.#model = model;
+    this.#year = year;
+  }
+
+  getInfo() {
+    return `${this.#year} ${this.#make} ${this.#model}`
+  }
 }
 
 class Car extends Vehicle {
   // YOUR CODE HERE
+
+  #doors
+
+  constructor(make, model, year, doors) {
+    super(make, model, year)
+
+    this.#doors = doors
+  }
+
+  getInfo() {
+    return `${super.getInfo()} with ${this.#doors} doors`
+  }
 }
 
 
